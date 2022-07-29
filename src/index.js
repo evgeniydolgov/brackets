03fb5c8d3baf916a8498b4683d9684c14e3d6ceb
evgeniydolgov <amyr214519@gmail.com>
2022-07-29 14:35:10 +0300

    init: solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let bracketsOpening = [];
+  let obj = [];
+  let arrBadBrackets = [];
+
+  bracketsConfig.forEach (item => {
+    if(item[0] === item [1]){
+      arrBadBrackets.push(item[0])
+    }
+  });
+
+  bracketsConfig.forEach(element => {
+    obj.push([...element])
+  });
+  obj.forEach(item => {
+    item.reverse();
+  });
+  obj = Object.fromEntries(obj);
+
+  for (let i = 0; i<bracketsConfig.length; i++) {
+    bracketsOpening.push(bracketsConfig[i][0]);
+  };
+
+  for (let i = 0; i<str.length; i++) {
+    let lastValue = stack[stack.length-1];
+    if (arrBadBrackets.includes(str[i])){
+      if(lastValue === str[i]){
+        stack.pop();
+      }else if (lastValue !== str[i]){
+        stack.push(str[i]);
+      }
+    } else if (bracketsOpening.includes(str[i])){
+      stack.push(str[i]);
+    } else {
+      if (stack.length === 0){
+      return false;
+      }
+      if (lastValue === obj[str[i]]) {
+        stack.pop();
+      }else{
+        return false;
+      }}};
+
+  return stack.length === 0;
 }
